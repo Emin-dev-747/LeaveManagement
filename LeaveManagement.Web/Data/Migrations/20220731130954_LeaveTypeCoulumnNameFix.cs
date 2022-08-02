65579c372cb56ebef5b6dc97564fd17e79ce3ecb
Em1N <emin.khudiyev.dev@gmail.com>
2022-08-02 16:20:14 +0400

    add crud Leave Management

---
+++ LeaveManagement.Web/Data/Migrations/20220731130954_LeaveTypeCoulumnNameFix.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LeaveManagement.Web.Data.Migrations
+{
+    public partial class LeaveTypeCoulumnNameFix : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DefultDays",
+                table: "LeaveTypes",
+                newName: "DefaultDays");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DefaultDays",
+                table: "LeaveTypes",
+                newName: "DefultDays");
+        }
+    }
+}
